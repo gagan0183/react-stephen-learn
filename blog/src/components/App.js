7213b415df6066683a58104ce87b23c73a220dbf
Gagandeep singh <g.deepsingh1@gmail.com>
2020-06-09 12:33:26 +0530

    actions create

--- blog/src/components/App.js
+++ blog/src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Postlist from "./Postlist";
 
 const App = () => {
-    return <div className="ui container">App</div>
+    return <div className="ui container">
+        <Postlist />
+    </div>
 };
 
 export default App;
