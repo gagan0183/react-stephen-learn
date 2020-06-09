b23b53b8d96ba7ac878644509e238d39a03d5c56
Gagandeep singh <g.deepsingh1@gmail.com>
2020-06-09 16:20:49 +0530

    user reducer

--- blog/src/components/Postlist.js
+++ blog/src/components/Postlist.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from "../actions";
+import UserHeader from "./Userheader";
 
 class Postlist extends React.Component {
     componentDidMount() {
@@ -17,6 +18,7 @@ class Postlist extends React.Component {
                             <h2>{post.title}</h2>
                             <p>{post.body}</p>
                         </div>
+                        <UserHeader userId={post.userId}/>
                     </div>
                 </div>
             );
