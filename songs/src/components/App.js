db83d402b72aeb556949c03ef5badfa1c8ef74a1
Gagandeep singh <g.deepsingh1@gmail.com>
2020-06-09 10:58:14 +0530

    app

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SongList from './Songlist';
+import Songdetail from './Songdetail';
 
 const App = () => {
     return (
@@ -7,6 +8,9 @@ const App = () => {
             <div className="ui row">
                 <div className="column eight wide">
                     <SongList />
+                </div>
+                <div className="column eight wide">
+                    <Songdetail />
                 </div>
             </div>
         </div>
