076067a045d6272fead9e668e740238d7bac45cf
Gagandeep singh <g.deepsingh1@gmail.com>
2020-06-10 18:05:58 +0530

    remove duplicate request

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,11 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
-import * as _ from 'lodash';
+import _ from 'lodash';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+}
 
 export const fetchPosts = () => {
     return async dispatch => {
@@ -11,14 +17,12 @@ export const fetchPosts = () => {
     }
 };
 
-export const fetchUser = (id) => dispatch =>  {
-    _fetchUser(id, dispatch);
+export const fetchUser = (id) =>  {
+    return async dispatch => {
+        const response = await jsonPlaceholder.get(`/users/${id}`);
+        dispatch({
+            type: 'FETCH_USER',
+            payload: response.data
+        });
+    }
 };
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({
-        type: 'FETCH_USER',
-        payload: response.data
-    });
-});
--- blog/src/components/Postlist.js
+++ blog/src/components/Postlist.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from "../actions";
+import { fetchPostsAndUsers } from "../actions";
 import UserHeader from "./Userheader";
 
 class Postlist extends React.Component {
     componentDidMount() {
-        this.props.fetchPosts()
+        this.props.fetchPostsAndUsers()
     }
 
     renderList() {
@@ -36,4 +36,4 @@ const mapStateToProps = state => {
     return { posts: state.posts }
 };
 
-export default connect(mapStateToProps, { fetchPosts })(Postlist);
+export default connect(mapStateToProps, { fetchPostsAndUsers })(Postlist);
